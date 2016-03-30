e5f0c629e02ffefa16d2548c6d4738a35f715236
chenshengwen <tarena01@tarena01.local>
2016-03-30 15:20:46 +0800

    初始化项目

---
+++ papakan/papakan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  papakan
+//
+//  Created by tarena01 on 16/3/30.
+//  Copyright © 2016年 陈圣文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ papakan/papakan/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  papakan
+//
+//  Created by tarena01 on 16/3/30.
+//  Copyright © 2016年 陈圣文. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
